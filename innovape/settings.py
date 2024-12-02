1cdcd7fe53a8d6634581b2e6d920e153571dbb85
Won Jae Kim <86081049+Daryu-Kim@users.noreply.github.com>
2024-12-02 13:19:39 +0000

    2024-12-02 fix

--- innovape/settings.py
+++ innovape/settings.py
@@ -124,11 +124,11 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / '_static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / '_media'
 
 
 # Default primary key field type
