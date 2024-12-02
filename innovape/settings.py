a79a5cd37598dde629b08d4b91de706b289c05a4
Won Jae Kim <86081049+Daryu-Kim@users.noreply.github.com>
2024-12-02 13:00:00 +0000

    2024-12-02

--- innovape/settings.py
+++ innovape/settings.py
@@ -124,11 +124,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
