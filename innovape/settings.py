e7f12e383a001c50f05b023eabf9262a715f490f
Won Jae Kim <86081049+Daryu-Kim@users.noreply.github.com>
2024-12-02 13:26:35 +0000

    2024-12-02 fix

--- innovape/settings.py
+++ innovape/settings.py
@@ -126,12 +126,13 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '_static')
+    os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT = BASE_DIR / '_static'
+
+STATIC_ROOT = os.path.join(BASE_DIR, '_static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
 
 
 # Default primary key field type
