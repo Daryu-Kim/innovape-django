75d43996ff955aa2a680f4e8a49e06b9f9c59c81
Daryu-Kim <dev_daryu@kakao.com>
2024-12-04 01:35:36 +0900

    2024-012-04

--- innovape/settings.py
+++ innovape/settings.py
@@ -25,7 +25,8 @@ DEFAULT_CHARSET = 'utf-8'
 SECRET_KEY = 'django-insecure-v*i#2y*&k$1xu9^@-shmne97up44+2(uriyh-5alsq!s3favxx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -153,3 +154,22 @@ MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'account.Member'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': '/path/to/your/django/error.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
