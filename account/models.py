b21fe31790c34429226be5f0f8203bf2c8d507ba
Daryu-Kim <dev_daryu@kakao.com>
2024-12-14 23:41:48 +0900

    2024-12-14

--- account/models.py
+++ account/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Member(AbstractUser):
     # 추가 필드를 정의할 수 있습니다.
-    phone_number = models.CharField(max_length=15, blank=True, null=True, unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
 
     def __str__(self):
         return self.username
