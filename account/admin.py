fa9952a04bd9787dc3a86f3b26856584a70aabe4
Daryu-Kim <dev_daryu@kakao.com>
2024-12-17 01:31:02 +0900

    2024-12-17

--- account/admin.py
+++ account/admin.py
@@ -6,10 +6,10 @@ class MemberAdmin(UserAdmin):
     model = Member
     # 필요한 필드를 추가합니다.
     fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('phone_number',)}),
+        (None, {'fields': ('phone_number', 'address_default', 'address_detail', 'address_code')}),
     )
     add_fieldsets = UserAdmin.add_fieldsets + (
-        (None, {'fields': ('phone_number',)}),
+        (None, {'fields': ('phone_number', 'address_default', 'address_detail', 'address_code')}),
     )
 
 admin.site.register(Member, MemberAdmin)
